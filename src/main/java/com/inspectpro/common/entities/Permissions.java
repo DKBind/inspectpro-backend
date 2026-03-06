b4a6575a6ba2effba90ff6562fd69421ee733de6
DKBind <dharmendra.bind@marvelsync.com>
2026-03-06 17:15:48 +0530

    folder structure added

---
+++ src/main/java/com/inspectpro/common/entities/Permissions.java
@@ -0,0 +1,19 @@
+package com.inspectpro.common.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "permissions")
+public class Permissions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
---
+++ src/main/java/com/inspectpro/common/entities/RolePermission.java
@@ -0,0 +1,27 @@
+package com.inspectpro.common.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role_permissions")
+public class RolePermission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Roles role;
+
+    @ManyToOne
+    @JoinColumn(name = "permission_id")
+    private Permissions permission;
+
+}
